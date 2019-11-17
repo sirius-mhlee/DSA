26472f086546188e75393e450e86013b99005c9a
Minho Lee <sirius.mhlee@gmail.com>
2019-11-17 10:19:00 +0900

    Add files via upload

---
+++ etc/bit_helper.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+namespace bit_helper
+{
+	template<typename value_type>
+	int find_most_left_bit_pos(const value_type& n)
+	{
+		return log2(n);
+	}
+
+    template<typename value_type>
+	int find_most_right_bit_pos(const value_type& n)
+	{
+		return log2(n & (-n));
+	}
+};
+
+//int mlb = bit_helper::find_most_left_bit_pos(n);
+//int mrb = bit_helper::find_most_right_bit_pos(n);
