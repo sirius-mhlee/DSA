8bf5a884ca115e8c76809ac7d90afad479745ad8
Minho Lee <sirius.mhlee@gmail.com>
2019-12-24 13:50:09 +0900

    Add files via upload

---
+++ etc/ter_search.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+template<typename value_type>
+class ter_search
+{
+public:
+    value_type result;
+
+public:
+    ter_search(value_type lo, value_type hi)
+        : result(0)
+    {
+        while (hi - lo >= 3)
+        {
+            value_type mid1 = (lo * 2 + hi) / 3;
+            value_type mid2 = (lo + hi * 2) / 3;
+
+            if (func(mid1) > func(mid2))
+            {
+                lo = mid1;
+            }
+            else
+            {
+                hi = mid2;
+            }
+        }
+
+        result = func(lo);
+        for (value_type x = lo + 1; x <= hi; x++)
+        {
+            result = min(result, func(x));
+        }
+    }
+
+public:
+    value_type func(value_type x)
+    {
+        return (x + 1);
+    }
+};
+
+//ter_search<long long> ts(lo, hi);
+//int ret = ts.result;
