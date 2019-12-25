56f2cc1d8986a4d0b7da3cd0cfbda92269fdf8a2
Minho Lee <sirius.mhlee@gmail.com>
2019-12-25 18:12:36 +0900

    Add files via upload

---
+++ etc/parallel_bi_search.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <algorithm>
+#include <functional>
+
+#include <vector>
+
+using namespace std;
+
+template<typename value_type>
+class parallel_bi_search
+{
+public:
+    class bi_search_info
+    {
+    public:
+        value_type lo;
+        value_type hi;
+
+    public:
+        bi_search_info(value_type _lo, value_type _hi)
+            : lo(_lo), hi(_hi)
+        {}
+    };
+
+public:
+    vector<bi_search_info> bs_info_list;
+    vector<value_type> result_list;
+
+public:
+    parallel_bi_search(int bi_search_size, value_type lo, value_type hi)
+        : bs_info_list(), result_list()
+    {
+        bs_info_list.assign(bi_search_size, bi_search_info(lo, hi));
+        result_list.assign(bi_search_size, -1);
+
+        while (true)
+        {
+            bool flag = false;
+
+            vector<vector<int>> cand_bs_info_inv(hi, vector<int>());
+
+            for (int i = 0; i < bi_search_size; i++)
+            {
+                if (bs_info_list[i].hi - bs_info_list[i].lo >= 2)
+                {
+                    flag = true;
+
+                    value_type mid = (bs_info_list[i].lo + bs_info_list[i].hi) / 2;
+                    cand_bs_info_inv[mid].push_back(i);
+                }
+            }
+
+            if (!flag) break;
+
+            for (value_type i = 0; i < hi; i++)
+            {
+                func(i);
+
+                for (int idx : cand_bs_info_inv[i])
+                {
+                    if (idx)
+                    {
+                        bs_info_list[idx].hi = i;
+                        result_list[idx] = i + 1;
+                    }
+                    else
+                    {
+                        bs_info_list[idx].lo = i;
+                    }
+                }
+            }
+        }
+    }
+
+public:
+    void func(value_type val)
+    {
+
+    }
+};
+
+//parallel_bi_search<long long> pbs(N, lo, hi);
+//long long ret = pbs.result_list[i];
