b4698f4a88963ed5e48f653e578caf32641bed97
Minho Lee <sirius.mhlee@gmail.com>
2020-01-11 21:35:53 +0900

    Add files via upload

--- etc/bit_helper.cpp
+++ etc/bit_helper.cpp
@@ -11,17 +11,17 @@ using namespace std;
 
 namespace bit_helper
 {
-    template<typename value_type>
-    int find_most_left_bit_pos(const value_type& n)
-    {
-        return log2(n);
-    }
+	template<typename value_type>
+	int find_most_left_bit_pos(const value_type& n)
+	{
+		return log2(n);
+	}
 
     template<typename value_type>
-    int find_most_right_bit_pos(const value_type& n)
-    {
-        return log2(n & (-n));
-    }
+	int find_most_right_bit_pos(const value_type& n)
+	{
+		return log2(n & (-n));
+	}
 };
 
 //int mlb = bit_helper::find_most_left_bit_pos(n);
---
+++ etc/mo_algorithm.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <algorithm>
+#include <functional>
+
+#include <vector>
+
+using namespace std;
+
+template<typename value_type>
+class mo_algorithm
+{
+public:
+	class query_info
+	{
+	public:
+		int left;
+		int right;
+
+		int idx;
+
+	public:
+		query_info(int _left, int _right, int _idx)
+			: left(_left), right(_right), idx(_idx)
+		{}
+	};
+
+public:
+	vector<value_type> data_list;
+
+	int query_unit_size;
+	vector<query_info> query_list;
+
+	value_type query_result;
+	vector<value_type> query_result_list;
+
+public:
+	mo_algorithm(int data_count)
+		: data_list(), query_unit_size(0), query_list(), query_result(0), query_result_list()
+	{
+		data_list.assign(data_count, 0);
+
+		query_unit_size = sqrt(data_count);
+	}
+
+public:
+	void add_query(int left, int right, int idx)
+	{
+		query_list.push_back(query_info(left, right, idx));
+	}
+
+	void include_query(value_type val)
+	{
+		query_result += val;
+	}
+
+	void exclude_query(value_type val)
+	{
+		query_result -= val;
+	}
+
+	void process()
+	{
+		query_result = 0;
+		query_result_list.assign(query_list.size(), 0);
+
+		sort(query_list.begin(), query_list.end(), [&](const query_info& l, const query_info& r)
+		{
+			int lidx = l.left / query_unit_size;
+			int ridx = r.left / query_unit_size;
+
+			if (lidx == ridx)
+			{
+				return l.right < r.right;
+			}
+			return lidx < ridx;
+		});
+
+		int left = 0;
+		int right = 0;
+		include_query(data_list[0]);
+
+		for (query_info curr : query_list)
+		{
+			while (left - 1 >= curr.left)
+			{
+				include_query(data_list[left - 1]);
+				left--;
+			}
+
+			while (right + 1 <= curr.right)
+			{
+				include_query(data_list[right + 1]);
+				right++;
+			}
+
+			while (left < curr.left)
+			{
+				exclude_query(data_list[left]);
+				left++;
+			}
+
+			while (right > curr.right)
+			{
+				exclude_query(data_list[right]);
+				right--;
+			}
+
+			query_result_list[curr.idx] = query_result;
+		}
+	}
+};
+
+//mo_algorithm<int> ma(N);
+//ma.data_list[i] = val;
+//ma.add_query(left, right, idx);
+//ma.process();
+//vector<int> ret = ma.query_result_list;
