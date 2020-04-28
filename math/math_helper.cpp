ded9fca98d33c266077b33761a57d4de11ec9679
Minho Lee <sirius.mhlee@gmail.com>
2020-04-28 21:04:18 +0900

    Add files via upload

--- math/math_helper.cpp
+++ math/math_helper.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <sstream>
 #include <limits>
+#include <numeric>
 #include <algorithm>
 #include <functional>
 
@@ -33,24 +34,45 @@ namespace math_helper
 	}
 
 	template<typename value_type>
+	map<value_type, int> factorization(value_type num)
+	{
+		map<value_type, int> factor_map;
+
+		for (value_type factor_num = 2; factor_num * factor_num <= num; factor_num++)
+		{
+			if (factor_num > num) break;
+			if (num % factor_num != 0) continue;
+
+			while (num % factor_num == 0)
+			{
+				num /= factor_num;
+				factor_map[factor_num]++;
+			}
+		}
+
+		if (num != 1) factor_map[num]++;
+
+		return factor_map;
+	}
+
+	template<typename value_type>
 	map<value_type, int> factorization(value_type num, const vector<value_type>& prime_list)
 	{
 		map<value_type, int> factor_map;
 
-		while (num > 1)
+		for (value_type prime_num : prime_list)
 		{
-			for (value_type prime_num : prime_list)
-			{
-				if (prime_num > num) break;
-				if (num % prime_num != 0) continue;
+			if (prime_num > num) break;
+			if (num % prime_num != 0) continue;
 
-				while (num % prime_num == 0)
-				{
-					num /= prime_num;
-					factor_map[prime_num]++;
-				}
+			while (num % prime_num == 0)
+			{
+				num /= prime_num;
+				factor_map[prime_num]++;
 			}
 		}
+
+		if (num != 1) factor_map[num]++;
 
 		return factor_map;
 	}
@@ -70,5 +92,6 @@ namespace math_helper
 //double pi_value = math_helper::pi;
 //long long mod_value = math_helper::mod<long long>(value, mod);
 //long long mod_inverse_value = math_helper::mod_inverse<long long>(value, mod);
+//map<long long, int> factor_map_naive = math_helper::factorization<long long>(value);
 //map<long long, int> factor_map = math_helper::factorization<long long>(value, prime_list);
 //long long power_value = math_helper::fast_power<long long>(value, power);
