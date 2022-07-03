60a2f4f703cfeedbc5f4ac3f4196a37d7f63c61d
sirius-mhlee <sirius.mhlee@gmail.com>
2022-07-03 15:55:15 +0900

    Update notation_convert

--- etc/notation_convert.cpp
+++ etc/notation_convert.cpp
@@ -27,6 +27,11 @@ public:
         {
             digit_value[ch] = ch - '0';
         }
+
+		for (char ch = 'A'; ch <= 'Z'; ch++)
+        {
+            digit_value[ch] = ch - 'A' + 10;
+        }
     }
 
 public:
